701ce7668f63dc79c8fd4462c21f0a6a4521fee9
Cabyca <cabyca@yandex.ru>
2022-04-03 23:51:51 +0300

    prosto5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
             \URL::forceScheme('https');
         }
 
-        Paginator::useBootstrap();
+        Paginator::useBootstrapFive();
+        Paginator::useBootstrapFour();
     }
 }
