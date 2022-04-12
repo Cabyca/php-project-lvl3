4a04fc563e4a590327ca1be570295aad28caa7c3
Cabyca <cabyca@yandex.ru>
2022-04-13 00:29:10 +0300

    commit4

--- tests/Feature/UrlCheckControllerTest.php
+++ tests/Feature/UrlCheckControllerTest.php
@@ -27,20 +27,21 @@ class UrlCheckControllerTest extends TestCase
     {
         $testHtml = file_get_contents(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', "Fixtures", 'test.html']));
 
-        if (!$testHtml) {
-            throw new \Exception("Cannot get content from fixture");
-        }
+        Http::fake(['https://www.yandex.ru' => Http::response($testHtml, 200)]);
 
-        Http::fake([
-            // Заглушка JSON ответа для адреса yandex.ru ...
-            'https://www.yandex.ru' => Http::response($testHtml, 200)
-        ]);
+        $expectedData = [
+            'h1' => 'Проанализировать страницу',
+            'title' => 'Анализатор страниц',
+            'description' => 'Description',
+            'url_id' => $this->id,
+            'status_code' => 200
+        ];
 
         $url = DB::table('urls')->first();
         /** @var object $url */
         $response = $this->post(route('checks.store', $url->id));
         $response->assertStatus(302);
         $response->assertSessionHasNoErrors();
-        $this->assertDatabaseHas('url_checks', ['h1' => 'Проанализировать страницу', 'title' => 'Анализатор страниц', 'description' => 'Description', 'url_id' => $this->id, 'status_code' => 200]);
+        $this->assertDatabaseHas('url_checks', $expectedData);
     }
 }
